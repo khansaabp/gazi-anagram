f45076dbc57de602373f8cea6b8131b68937d204
khansaabp <gazikhan747@gmail.com>
2023-03-30 15:30:21 +0530

    UpdateApp.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Anagram } from "./components/Anagram";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
+const LazyStory=React.lazy(()=>import ("./components/Story"))
+const LazyStoryDetails=React.lazy(()=>import("./components/StoryDetails"))
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Anagram />
+      <br />
+      <BrowserRouter>
+      <React.Suspense fallback="Loading">
+      <Routes>
+     
+        <Route path="/" element={<LazyStory/>}/>
+      <Route path="/story" element={<LazyStoryDetails/>}/>
+      
+      </Routes>
+      </React.Suspense>
+      </BrowserRouter>
+      
+      
     </div>
   );
 }
